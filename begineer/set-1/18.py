36bb898c7026ba5d2407483a875e997bbef0837b
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-04 15:18:15 +0530

    Add files via upload

---
+++ begineer/set-1/18.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())  
+
+for num in range(a+1,b):
+    temp = num
+    sum = 0
+    while temp > 0:  
+       digit = temp % 10  
+       sum += digit ** 3  
+       temp //= 10
+    if num == sum:  
+       print(num,end=" ")
+    
