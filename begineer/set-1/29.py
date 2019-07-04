57800e543b667b30fbd8be77dd66a814b5ecf6b7
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-04 23:40:02 +0530

    Update 29.py

--- begineer/set-1/29.py
+++ begineer/set-1/29.py
@@ -1,9 +1,12 @@
 a,b=map(int,input().split())
+c = 0
 for i in range(a,b+1):
    if i>1:
         for x in range(2,i):
             if i%x==0:
                  break
         else:
-            print(i,end=" ")
+            c+=1
+        
+print(c)
         
