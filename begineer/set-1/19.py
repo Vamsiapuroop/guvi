e7e45297b087e29f62b46e6f0b9d0f02c0c94c1f
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-04 15:35:54 +0530

    Update 19.py

--- begineer/set-1/19.py
+++ begineer/set-1/19.py
@@ -1,7 +1,7 @@
-n=int(input())
-def fact(n):
-    if n==1:
+a=int(input())
+def fact(a):
+    if a==1:
         return 1
     else:
-         return n * fact(n - 1)
-print(fact(n))
+        return a * fact(a - 1)
+print(fact(a))
