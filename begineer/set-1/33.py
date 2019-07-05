d9c81e5786acf98bcd4da39f714fbe36e75bb3a9
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-05 15:05:24 +0530

    Add files via upload

---
+++ begineer/set-1/33.py
@@ -0,0 +1,16 @@
+n=int(input())
+l=list(map(int,input().split()))
+z=[]
+for i in range(len(l)-1):
+    if l[i]==i:
+        z.append(i)
+        z.sort()
+
+if len(z)>0:
+    for x in z:
+        print(x,end="")
+else:
+    print("-1")
+            
+        
+    
