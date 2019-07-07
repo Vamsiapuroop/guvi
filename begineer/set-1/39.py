858a8a0bfd55fb5ef7edfaed24f0c206064e789d
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-07 21:05:57 +0530

    Add files via upload

--- begineer/set-1/39.py
+++ begineer/set-1/39.py
@@ -1,5 +1,7 @@
 n=int(input())
 l=list(map(int,input().split()))
-for i in range(len(l)-1):
-    if l[i]+l[i+1]==0 or l[i]+l[i+1]==-1 or l[i]+l[i+1]==1:
-        print(l[i],end=" ")
+a=sorted(l)
+print(l[0],l[1])
+        
+        
+        
