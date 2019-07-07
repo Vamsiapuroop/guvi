495882b2e39c145ee347fcdf26206a312882cc61
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-07 21:26:09 +0530

    Add files via upload

--- begineer/set-1/39.py
+++ begineer/set-1/39.py
@@ -1,7 +1,10 @@
-n=int(input())
-l=list(map(int,input().split()))
-a=sorted(l)
-print(l[0],l[1])
-        
-        
-        
+a=int(input(""))
+n=list(map(int,input().split()))
+large=max(n)
+y,z=0,0
+for i in range(0,len(n)):
+ for j in range(i+1,len(n)):
+  if abs(n[i]+n[j])< large:
+   y,z=n[i],n[j]
+   large=abs(y+z)
+print(y, z)
