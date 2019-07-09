73c8a857a8e2d86733b08fefdb3ff1d8913f71f8
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-09 17:52:32 +0530

    Add files via upload

---
+++ begineer/set-1/h10.py
@@ -0,0 +1,8 @@
+n,m=map(int,input().split())
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+if set(B).issubset(set(A)):
+    print("YES")       
+else:
+    print("no")
+       
