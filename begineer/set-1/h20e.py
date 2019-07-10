30784ef0a1ac1b16383626ff8ffe3f312c6be0b7
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-11 01:46:15 +0530

    Update h20e.py

--- begineer/set-1/h20e.py
+++ begineer/set-1/h20e.py
@@ -1,19 +1,19 @@
-a1,b1=map(int,input().split())
+v,a=map(int,input().split())
 c=[]
 d=0
-k=[]
-for i in range(a1,b1+1):
+l=[]
+for i in range(v,a+1):
         c.append(bin(i)[2::])
 for i in range(0,len(c)):
-        k.append(c[i].count("1"))
+        l.append(c[i].count("1"))
  
-for i in range(0,len(k)):
-        if k[i]!=1 and k[i]!=2:
-                for p in range(2,k[i]):
-                        if k[i]%p==0:
+for i in range(0,len(l)):
+        if l[i]!=1 and l[i]!=2:
+                for p in range(2,l[i]):
+                        if l[i]%p==0:
                                 break
-                if p==k[i]-1:
+                if p==l[i]-1:
                         d=d+1
-        elif k[i]==2:
+        elif l[i]==2:
                 d=d+1
 print(d)
