f7ed7b933f3f2cf74fd20c3e0b93cb9062cd6009
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-13 00:46:52 +0530

    Update p17.py

--- begineer/set-1/p17.py
+++ begineer/set-1/p17.py
@@ -1,9 +1,9 @@
 n,k=map(int,input().split())
-l=list(map(int,input().split()))
+li=list(map(int,input().split()))
 a=0
 for i in range(0,n):
     for j in range(1,n):
-        if l[i]+l[j]==k and i!=j:
+        if li[i]+li[j]==k and i!=j:
             a=1
             break
-print("YES" if a else "NO")
+print("yes" if a else "no")
