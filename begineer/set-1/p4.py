8962e10cc4a5f0906bfba5535367926108b47762
Vamsiapuroop <36064390+Vamsiapuroop@users.noreply.github.com>
2019-07-14 23:14:49 +0530

    Update p4.py

--- begineer/set-1/p4.py
+++ begineer/set-1/p4.py
@@ -1,11 +1,11 @@
-p,q = map(str,input().split())
+a,b = map(str,input().split())
 m = 0
-if len(p)>len(q):
-	p,q=q,p
+if len(a)>len(b):
+	a,b=b,a
 x = 0
-while x<len(p):
-	  m += (ord(q[x])-ord(p[x]))
+while x<len(a):
+	  m += (ord(b[x])-ord(a[x]))
 	  x += 1
-for x in range(x,len(q)):
-	  m += ord(q[x])-ord('p')+1
+for x in range(x,len(b)):
+	  m += ord(b[x])-ord('a')+1
 print(m)
